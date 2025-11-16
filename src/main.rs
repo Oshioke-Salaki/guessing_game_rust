1e34f9e2cb4a0dcf35c4dfbc287ac5a88d32f434
Oshioke-Salaki <salaki1902@gmail.com>
2025-11-16 21:12:52 +0100

    chore: fmt

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,36 @@
+use rand::Rng;
 use std::cmp::Ordering;
 use std::io; //This imports the input/output library from the standard library
-use rand::Rng;
 
 fn main() {
     println!("Welcome to the guess the number game by Oshioke Salaki!");
     let secret_number = rand::thread_rng().gen_range(1..=100); // Used to generate a random number between 1 to 100.
     let mut total_attempts: u32 = 0;
-    
-    loop{
+
+    loop {
         if total_attempts == 0 {
             println!("Please enter your guess.");
-        }else{
+        } else {
             println!("Try again, enter your guess.");
         }
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
-        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
         // let guess: u32 = guess.trim().parse().expect("Please enter a number");
-        let guess: u32 = match guess.trim().parse(){
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue
+            Err(_) => continue,
         };
-        
-        
-        match guess.cmp(&secret_number){
+
+        match guess.cmp(&secret_number) {
             Ordering::Greater => println!("Oops, that's too big!"),
             Ordering::Less => println!("Oops, that's too small!"),
             Ordering::Equal => {
-               break println!("Woohoo! You win!");
-            },
+                break println!("Woohoo! You win!");
+            }
         }
         total_attempts += 1;
     }
-
 }
